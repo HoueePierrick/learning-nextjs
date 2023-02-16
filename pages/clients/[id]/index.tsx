7025abe15f8e451facfac28c1d2d1ec4a8dc6af0
PierrickHOUEE <houee.pierrick@gmail.com>
2023-02-16 19:39:33 +0100

    Finished the first section

--- pages/clients/[id]/index.tsx
+++ pages/clients/[id]/index.tsx
@@ -5,9 +5,21 @@ function ClientsProjectsPage() {
 
   console.log(router.query);
 
+  function loadProjectHandler() {
+    // Navigating to an other page
+    // router.push("/clients/max/projecta");
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: { id: "max", clientprojectid: "projecta" },
+    });
+    // Replace current page with this one (can't go back)
+    // router.replace("/clients/max/projecta");
+  }
+
   return (
     <div>
       <h1>The projects of a given client</h1>
+      <button onClick={loadProjectHandler}>Load project A</button>
     </div>
   );
 }
